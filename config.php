e408389c040d247b7fd646a06c693b2a939e1503
Chindy <chindyajj@gmail.com>
2025-12-18 20:29:12 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$config = [ 
-    'host' => 'localhost', 
-    'username' => 'root', 
-    'password' => '', // Sesuaikan dengan password database kamu 
-    'db_name' => 'latihan_oop' 
-]; 
-?>
